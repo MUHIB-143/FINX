5a0b5205c7d55ec41789163ec05b3e0b7c1fb5f9
MARUFUL HAQUE MUHIB <122245772+MUHIB-143@users.noreply.github.com>
2023-10-27 12:08:16 +0600

    Delete FINX.p

--- FINX.py
+++
@@ -1,8 +0,0 @@
-import os,platform
-os.system('git pull')
- 
-acvh=platform.architecture()[0]
-if acvh=="32bit":
-    os.system('chmod 777 FINX32;./FINX32')
-elif acvh=="64bit":
-    os.system('chmod 777 FINX64;./FINX64')
