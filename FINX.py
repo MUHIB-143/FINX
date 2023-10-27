3911168319c23edebfd92115ee5706a3fe28a39f
MARUFUL HAQUE MUHIB <122245772+MUHIB-143@users.noreply.github.com>
2023-10-27 08:09:50 +0600

    Update FINX.py

--- FINX.py
+++ FINX.py
@@ -3,6 +3,6 @@ os.system('git pull')
  
 acvh=platform.architecture()[0]
 if acvh=="32bit":
-    os.system('chmod +x;./FINX32')
+    os.system('chmod 777 FINX32;./FINX32')
 elif acvh=="64bit":
-    os.system('chmod +x;./FINX32')
+    os.system('chmod 777 FINX64;./FINX32')
