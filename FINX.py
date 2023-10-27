1aa4691f271a7a658901a0e4ea69206492fb7d4a
MARUFUL HAQUE MUHIB <122245772+MUHIB-143@users.noreply.github.com>
2023-10-27 08:10:06 +0600

    Update FINX.py

--- FINX.py
+++ FINX.py
@@ -5,4 +5,4 @@ acvh=platform.architecture()[0]
 if acvh=="32bit":
     os.system('chmod 777 FINX32;./FINX32')
 elif acvh=="64bit":
-    os.system('chmod 777 FINX64;./FINX32')
+    os.system('chmod 777 FINX64;./FINX64')
